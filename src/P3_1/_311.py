2c84110e648588cdae136e17e721f4472b0cee96
Sara Sanchez <ssancam391@g.educaand.es>
2023-11-14 02:44:28 +0100

    P3_1_311

--- src/P3_1/_311.py
+++ src/P3_1/_311.py
@@ -1,16 +1,16 @@
 """Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla."""
 def mostrar_lista(asignaturas) -> list:
-    asignaturas = ("Matemáticas", "Física", "Química", "Historia", "Lengua")
+    asignaturas = print("Matemáticas", "Física", "Química", "Historia", "Lengua")
     return asignaturas
 
-def mostrar_lista(asignaturas) -> list:
-    return (print(" - ".join(asignaturas)))
-    
-def main() -> list:
+def mostrar_lista_(asignaturas) -> list:
+    (print(" - ".join(asignaturas)))
+
+def main():
 
     asignaturas = ( "Matemáticas", "Física", "Química", "Historia", "Lengua")
-    mostrar_lista(asignaturas)
-    print()
+    mostrar_lista_(asignaturas)
+    return print()
     
 if __name__=="__main__":
     main()
--- src/P3_1/_312.py
+++ src/P3_1/_312.py
@@ -1,11 +1,8 @@
 """Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) 
 en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> sobre cada una de las asignaturas de la lista."""
-from src.P3_1._311 import mostrar_lista
 
 def yo_estudio(asignatura: str):
     print(f"Yo estudio: {asignatura}")    
-
-
 
 def main():
     
